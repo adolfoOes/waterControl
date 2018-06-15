9cdc233602e1163b6f516b4bb3edd7f8140eaa32
Adolfo Oestereich <adolfo.oes@gmail.com>
2018-06-15 11:45:12 -0300

    mongo changes

--- controllers/sensorBoxDatasController.js
+++ controllers/sensorBoxDatasController.js
@@ -1,14 +1,49 @@
 module.exports = function(app){
 
     app.get('/findAllSensorBoxDatas',function(req,res){
-        console.log("List all datas.")
-
+       
         var dataBase = app.database.connectionFactory;
         var sensorBoxDatas = dataBase.Mongoose.model('waterBoxDatas', dataBase.SensorBoxDatasSchema, 'waterBoxDatas');
 
-        sensorBoxDatas.find({}).lean().exec(function(error,docs){
-            console.log(docs)
+        sensorBoxDatas.find({}).exec(function(error,docs){
+            
+            for (var sensorBoxData in docs){
+                console.log(docs[sensorBoxData].id)
+            }
+            
+        })
+    });
+
+    app.post('/newSensorBoxData', function(req,res){
+
+        console.log("New cad.")
+
+        var sensorBoxDatas = dataBase.Mongoose.model('waterBoxDatas', dataBase.SensorBoxDatasSchema, 'waterBoxDatas');
+
+        var newData = new sensorBoxDatas({
+            id : req.body.id, 
+            name : req.body.name,
+            percentageOfWater : req.body.percentageOfWater,
+            flowPerMinute : req.body.flowPerMinute,
+            date : req.body.date
+        });
+
+        console.log(newData);
+
+        newData.save(function(error){
+            if (error){
+                console.log('error');
+                res.status(500).json({error : error.message});
+                res.end();
+                return;
+            }
+            console.log('OK');
+            res.json(newData);
+            res.end();
         })
 
-    });
+        
+
+    })
+
 }
